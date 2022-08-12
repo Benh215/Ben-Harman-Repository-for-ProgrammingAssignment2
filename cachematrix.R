28876c725379e724f1a767db3687208c7f76f561
Benh215 <harmanben@hotmail.com>
2022-08-11 23:18:22 -0400

    getting ready to push cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## set function in parent environment takes argument x and stores it as input via y
+## set function also defines value for solve as empty (NULL) and stores 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+## get function retrieves value x from parent environment
+## setsolve function uses solve function to invert the input (x) matrix
+## getsolve function defines m as matrix input from the parent environment, which should be null
+## setsolve function uses solve function to invert the input (x) matrix
+## getsolve function defines m as matrix input from the parent environment, which should be null
+        
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## cachesolve takes input x and attempts to retrieve an inverted array stored using the getsolve object
+## if m is not null it prints the message "getting cached data" and returns the inverted array from the cache
+## if m is not in the cache, input is stored as "data" object and input array is inverted
+## inverted matrix is committed to cache and printed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
